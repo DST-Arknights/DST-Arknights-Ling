67c5fd82c52c6be8935c5b89ffe7b8bda1571418
望月心灵 <TohsakaKuro@outlook.com>
2026-02-25 13:00:36 +0800

    feat: 添加已知位置组件，优化云山亭传输逻辑

--- scripts/components/ling_cloud_pavilion_transfer.lua
+++ scripts/components/ling_cloud_pavilion_transfer.lua
@@ -1,32 +1,16 @@
 local CONSTANTS = require "ark_constants_ling"
+
+local function GetEnterKnownLocationKey()
+  return "cloudpavilion_enter_" .. tostring(TheShard:GetShardId())
+end
+
+local function GetEnterEntityKey()
+  return "cloudpavilion_enter_" .. tostring(TheShard:GetShardId())
+end
+
 local LingCloudPavilionTransfer = Class(function(self, inst)
     self.inst = inst
-    self.enterPos = nil
-    self.enterInst = nil
 end)
-
-function LingCloudPavilionTransfer:OnSave()
-    local data = {}
-    if self.enterPos then
-        data.enterPos = self.enterPos
-    end
-    if self.enterInst then
-        data.enterInst = self.enterInst.GUID
-    end
-    return data, data.enterInst and {data.enterInst} or nil
-end
-
-function LingCloudPavilionTransfer:OnLoad(data)
-    if data.enterPos then
-        self.enterPos = data.enterPos
-    end
-end
-
-function LingCloudPavilionTransfer:LoadPostPass(newents, savedata)
-    if savedata.enterInst then
-        self.enterInst = newents[savedata.enterInst]
-    end
-end
 
 function LingCloudPavilionTransfer:CanEnterCloudPavilion(enterInst)
   -- 只有主世界才能进入云山亭
@@ -64,26 +48,33 @@ function LingCloudPavilionTransfer:EnterCloudPavilion(enterInst)
   if not target or not target:IsValid() then
     return
   end
-  local x, y, z = target.Transform:GetWorldPosition()
-  if enterInst then
-    self.enterInst = enterInst
-    local enterX, enterY, enterZ = enterInst.Transform:GetWorldPosition()
-    self.enterPos = { enterX, enterY, enterZ }
-  else
-    self.enterInst = nil
-    local enterX, enterY, enterZ = self.inst.Transform:GetWorldPosition()
-    self.enterPos = { enterX, enterY, enterZ }
+
+  if not self.inst.components.knownlocations then
+    self.inst:AddComponent("knownlocations")
   end
-  Teleport(self.inst, {x, y, z})
+  if not self.inst.components.entitytracker then
+    self.inst:AddComponent("entitytracker")
+  end
+  local enterPos
+  if enterInst then
+    self.inst.components.entitytracker:TrackEntity(GetEnterEntityKey(), enterInst)
+    enterPos = enterInst:GetPosition()
+  else
+    self.inst.components.entitytracker:ForgetEntity(GetEnterEntityKey())
+    enterPos = self.inst:GetPosition()
+  end
+  self.inst.components.knownlocations:RememberLocation(GetEnterKnownLocationKey(), enterPos)
+  Teleport(self.inst, target:GetPosition())
 end
 
 function LingCloudPavilionTransfer:ExitCloudPavilion()
-  if not self.enterPos then
+  local enterPos = self.inst.components.knownlocations:GetLocation(GetEnterKnownLocationKey())
+  if not enterPos then
     return
   end
-  Teleport(self.inst, self.enterPos)
-  self.enterPos = nil
-  self.enterInst = nil
+  Teleport(self.inst, enterPos)
+  self.inst.components.knownlocations:ForgetLocation(GetEnterKnownLocationKey())
+  self.inst.components.entitytracker:ForgetEntity(GetEnterEntityKey())
 end
 
 return LingCloudPavilionTransfer
