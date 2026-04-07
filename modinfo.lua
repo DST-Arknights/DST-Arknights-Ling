ade0c53342a30701c56c762f634324a257dd71e3
望月心灵 <TohsakaKuro@outlook.com>
2026-04-07 16:49:56 +0800

    release: 更新版本号至0.2.1

--- modinfo.lua
+++ modinfo.lua
@@ -25,7 +25,7 @@ QQ群: 666511586
 欢迎大家积极参与!]]
 })
 author = "让 望月心灵"
-version = "0.2.0"
+version = "0.2.1"
 forumthread = "https://github.com/TohsakaKuro/DST-Arknights-Typhon/issues"
 
 api_version = 10
